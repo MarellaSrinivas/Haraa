9bca1122c223d691f6d9396f691652ea39f46908
MarellaSrinivas <161401193+MarellaSrinivas@users.noreply.github.com>
2025-09-04 11:24:08 +0530

    Update Footer.tsx
    what's app image added again

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -144,7 +144,7 @@ const Footer = () => {
         </div>
 
              <a href="https://wa.me/+918019180099" className="whatsapp-button" target="_blank" title="Chat on WhatsApp">
-                <img src="../../assets/whatsapp.png" width="60px" height="60px" alt="WhatsApp" />
+                <img src="@/assets/whatsapp.png" width="60px" height="60px" alt="WhatsApp" />
               </a> 
       </div>
     </footer>
